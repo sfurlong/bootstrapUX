d933a98dbd907c9f203fa21788421d2274bae6d3
Furlong, Steve <furlons@amazon.com>
2017-12-26 15:25:09 -0500

    first rel

--- furcommon.js
+++ furcommon.js
@@ -1,3 +1,37 @@
 function myFunction() {
-    alert("hello world");
+	var apigClient = apigClientFactory.newClient();
+	
+	var params = {
+		    //This is where any header, path, or querystring request params go. The key is the parameter named as defined in the API
+		    param0: '',
+		    param1: '',
+			furQueryString: 'value3'
+		};
+	var body = {
+		    //This is where you define the body of the request
+		};
+	//var body = "";
+	var additionalParams = {
+		    //If there are any unmodeled query parameters or headers that need to be sent with the request you can add them here
+			/*
+		    headers: {
+		        param0: '',
+		        param1: ''
+		    },
+		    queryParams: {
+		        param0: '',
+		        param1: ''
+		    }
+		    */
+		};
+
+	apigClient.helloworldGet(params, body, additionalParams)
+		    .then(function(result){
+		        //This is where you would put a success callback
+		    		console.log("SUCCESS: " + JSON.stringify(result));
+		    }).catch( function(result){
+		        //This is where you would put an error callback
+		    		console.log("ERROR: " + JSON.stringify(result));
+		    });	
+    //alert("hello world: " + result);
 }
